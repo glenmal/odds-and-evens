023927d4194d7cc7305c6464d9671ec84e6cc791
Glen <glenmalbog30@gmail.com>
2024-05-16 22:33:32 +1200

    Added functions to show current game stats and ending a game. Also added functionality to check if there is an active game, and gives an error message if there user tries to input a command related to an active game that doesn't exist (play, showstats, endgame).Passed all task 5 test cases

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -3,31 +3,15 @@ package nz.ac.auckland.se281;
 import nz.ac.auckland.se281.Main.Choice;
 import nz.ac.auckland.se281.Main.Difficulty;
 
-/**
- * OOP Requirements:
- *
- * <p>strategy design pattern for the 3 different AI's strategies (Random and Top) and change them
- * at runtime
- *
- * <p>Also you should use the Factory design pattern to create the different AI instances based on
- * the difficulty level given by the player (EASY, MEDIUM, or HARD).
- *
- * <p>For the Strategy design pattern make sure there is the method setStrategy as we did in class
- * (call the meothod exactly like this).
- *
- * <p>You should use at least 1 interface and 1 abstract class or at least 2 abstract classes or at
- * least 2 interfaces. This includes those interfaces/abstract classes that you will need to create
- * to implement the design patterns.
- */
-
 /** This class represents the Game is the main entry point. */
 public class Game {
   private Integer roundCount = 0;
-  private String playerName = "";
+  private String playerName = null;
   private AI botAI;
   private Choice playerChoice;
   private Integer oddsCount;
   private Integer evenCount;
+  private Integer winCount;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // generates bot to play against
@@ -39,42 +23,86 @@ public class Game {
     roundCount = 0;
     oddsCount = 0;
     evenCount = 0;
+    winCount = 0;
   }
 
   public void play() {
-    this.roundCount++;
-    Integer botFingers = botAI.botOutput(roundCount, evenCount, oddsCount, playerChoice);
-    MessageCli.START_ROUND.printMessage(Integer.toString(roundCount));
-    MessageCli.ASK_INPUT.printMessage();
-    String input = Utils.scanner.nextLine();
-    // checks if user doesn't input a correct value and keeps asking user until they input a correct
-    // value
-    while (Inputs.checkFingerErrors(input)) {
-      MessageCli.INVALID_INPUT.printMessage();
-      input = Utils.scanner.nextLine();
-    }
-    MessageCli.PRINT_INFO_HAND.printMessage(this.playerName, input);
-    MessageCli.PRINT_INFO_HAND.printMessage("HAL-9000", Integer.toString(botFingers));
+    // if statement checks if there is an active game
+    if (playerName != null) {
+      this.roundCount++;
+      Integer botFingers = botAI.botOutput(roundCount, evenCount, oddsCount, playerChoice);
+      MessageCli.START_ROUND.printMessage(Integer.toString(roundCount));
+      MessageCli.ASK_INPUT.printMessage();
+      String input = Utils.scanner.nextLine();
+      // checks if user doesn't input a correct value and keeps asking user until they input a
+      // correct
+      // value
+      while (Inputs.checkFingerErrors(input)) {
+        MessageCli.INVALID_INPUT.printMessage();
+        input = Utils.scanner.nextLine();
+      }
+      MessageCli.PRINT_INFO_HAND.printMessage(this.playerName, input);
+      MessageCli.PRINT_INFO_HAND.printMessage("HAL-9000", Integer.toString(botFingers));
 
-    // adds to even count or odd count
-    if (Integer.parseInt(input) % 2 == 0) {
-      evenCount++;
-    } else {
-      oddsCount++;
-    }
+      // adds to even count or odd count
+      if (Integer.parseInt(input) % 2 == 0) {
+        evenCount++;
+      } else {
+        oddsCount++;
+      }
 
-    // checks if the player won
-    Integer sum = Integer.parseInt(input) + botFingers;
-    if (Inputs.winCalc(Integer.parseInt(input), botFingers, this.playerChoice)) {
-      MessageCli.PRINT_OUTCOME_ROUND.printMessage(
-          Integer.toString(sum), Inputs.oddsWinner(sum), playerName);
+      // checks if the player won
+      Integer sum = Integer.parseInt(input) + botFingers;
+      if (Inputs.winCalc(Integer.parseInt(input), botFingers, this.playerChoice)) {
+        MessageCli.PRINT_OUTCOME_ROUND.printMessage(
+            Integer.toString(sum), Inputs.oddsWinner(sum), playerName);
+        winCount++;
+      } else {
+        MessageCli.PRINT_OUTCOME_ROUND.printMessage(
+            Integer.toString(sum), Inputs.oddsWinner(sum), "HAL-9000");
+      }
     } else {
-      MessageCli.PRINT_OUTCOME_ROUND.printMessage(
-          Integer.toString(sum), Inputs.oddsWinner(sum), "HAL-9000");
+      MessageCli.GAME_NOT_STARTED.printMessage();
     }
   }
 
-  public void endGame() {}
+  // adds function to end game
+  public void endGame() {
+    // if statement checks if there is an active game
+    if (playerName != null) {
+      Integer botWins = this.roundCount - this.winCount;
+      showStats();
+      if (winCount == botWins) {
+        MessageCli.PRINT_END_GAME_TIE.printMessage();
+      } else if (botWins > winCount) {
+        MessageCli.PRINT_END_GAME.printMessage("HAL-9000");
+      } else {
+        MessageCli.PRINT_END_GAME.printMessage(playerName);
+      }
+      // resets game conditions
+      this.botAI = null;
+      this.playerChoice = null;
+      this.playerName = null;
+      roundCount = 0;
+      oddsCount = 0;
+      evenCount = 0;
+      winCount = 0;
+    } else {
+      MessageCli.GAME_NOT_STARTED.printMessage();
+    }
+  }
 
-  public void showStats() {}
+  // adds function to show stats of current game
+  public void showStats() {
+    // if statement checks if there is an active game
+    if (playerName != null) {
+      Integer botWins = this.roundCount - this.winCount;
+      MessageCli.PRINT_PLAYER_WINS.printMessage(
+          playerName, Integer.toString(winCount), Integer.toString(roundCount - winCount));
+      MessageCli.PRINT_PLAYER_WINS.printMessage(
+          "HAL-9000", Integer.toString(botWins), Integer.toString(roundCount - botWins));
+    } else {
+      MessageCli.GAME_NOT_STARTED.printMessage();
+    }
+  }
 }
